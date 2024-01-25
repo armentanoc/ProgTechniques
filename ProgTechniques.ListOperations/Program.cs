6b556a1400a01cbfbae64fac772c359fb946f8e8
Carolina Armentano <armentanocarolina@gmail.com>
2024-01-25 17:08:34 -0300

    refactor: making string input default in list operations.

--- ProgTechniques.ListOperations/Program.cs
+++ ProgTechniques.ListOperations/Program.cs
@@ -6,37 +6,36 @@ namespace ProgTechniques.ListOperations
     {
         static void Main(string[] args)
         {
-
-            //var input = 
-            //    GenericValues<string>
-            //    .GenerateList
-            //    (
-            //    "Idiossincrasia",
-            //    "Ambivalente",
-            //    "Quimérica",
-            //    "Perpendicular",
-            //    "Efêmero",
-            //    "Pletora",
-            //    "Obnubilado",
-            //    "Xilografia",
-            //    "Quixote",
-            //    "Inefável"
-            //    );
-
             const int MIN_LENGTH = 10;
 
             var input =
-                GenericValues<long>
+                GenericValues<string>
                 .GenerateList
                 (
-                12345678912,
-                1234567812,
-                123,
-                12,
-                12345678912345,
-                12345,
-                124467689790
+                "Idiossincrasia",
+                "Ambivalente",
+                "Quimérica",
+                "Perpendicular",
+                "Efêmero",
+                "Pletora",
+                "Obnubilado",
+                "Xilografia",
+                "Quixote",
+                "Inefável"
                 );
+
+            //var input =
+            //    GenericValues<long>
+            //    .GenerateList
+            //    (
+            //    12345678912,
+            //    1234567812,
+            //    123,
+            //    12,
+            //    12345678912345,
+            //    12345,
+            //    124467689790
+            //    );
 
             var output = FilterListByMinLength(input, MIN_LENGTH);
             PrintEnumerable(output);
