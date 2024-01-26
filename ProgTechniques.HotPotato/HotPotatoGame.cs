0eb2851837adf49df9d445e18450d1ea93963001
Carolina Armentano <armentanocarolina@gmail.com>
2024-01-26 09:31:09 -0300

    feat: hot potato game (queue operations).

---
+++ ProgTechniques.HotPotato/HotPotatoGame.cs
@@ -0,0 +1,48 @@
+﻿
+namespace ProgTechniques.HotPotato
+{
+    internal class HotPotatoGame
+    {
+        private static Random _random = new Random();
+        internal static void Play(int numberOfPlayers)
+        {
+            Queue<string> playersQueue = InitializePlayersQueue(numberOfPlayers);
+            int passesUntilExplode = GetRandomPassesUntilExplode();
+
+            Console.WriteLine($"Simulando jogo de batata quente com {numberOfPlayers} jogadores.");
+            Console.WriteLine($"A batata vai explodir após {passesUntilExplode} passes.");
+
+            PerformPassesUntilExplode(playersQueue, passesUntilExplode);
+        }
+
+        static Queue<string> InitializePlayersQueue(int numberOfPlayers)
+        {
+            var playersQueue = new Queue<string>();
+
+            for (int i = 1; i <= numberOfPlayers; i++)
+            {
+                playersQueue.Enqueue($"Player {i}");
+            }
+
+            return playersQueue;
+        }
+
+        static int GetRandomPassesUntilExplode()
+        {
+            return _random.Next(1, 101);
+        }
+
+        static void PerformPassesUntilExplode(Queue<string> playersQueue, int passesUntilExplode)
+        {
+            for (int passesCount = 1; passesCount <= passesUntilExplode; passesCount++)
+            {
+                string currentPlayer = playersQueue.Dequeue();
+                Console.WriteLine($"A batata está passando por {currentPlayer} e esse é o passe {passesCount}.");
+                playersQueue.Enqueue(currentPlayer);
+            }
+
+            string eliminatedPlayer = playersQueue.Dequeue();
+            Console.WriteLine($"A batata explodiu! {eliminatedPlayer} está fora do jogo.");
+        }
+    }
+}
---
+++ ProgTechniques.HotPotato/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ProgTechniques.HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NUMBER_OF_PLAYERS = 10;
+            HotPotatoGame.Play(NUMBER_OF_PLAYERS);
+        }
+    }
+}
